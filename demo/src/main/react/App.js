ba69d94642205c1e8abc485da57580a2940fa257
Nairu Guan <guan.nairu@gmail.com>
2024-02-18 16:06:30 -0500

    class and functional components, lifecycle method intro

--- demo/src/main/react/App.js
+++ demo/src/main/react/App.js
@@ -1,40 +1,47 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
+import AppContainer from "/containers/AppContainer.js"
 
 export class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            obj: "Ball",
+            count: 0
+        }
+    }
+
+
 
     render() {
+        const { obj, count } = this.state
+        const increment = () => {
+            this.setState({ count: this.state.count + 1 })
+        }
         return (
-            <>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Features</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Pricing</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <h1>Hello, world!</h1>
-            </>
+            <AppContainer
+                name="Jose"
+                color="Green"
+                // object={this.state.obj}
+                object={obj}
+                count={count}
+                increment={increment}
+            />
         )
     }
 }
 
 ReactDom.render(<App />, document.querySelector("#app"));
+
+
+// Notes:
+
+//lifecycle methods:
+// componentDidMount, runs once when component loads into DOM
+//      API calls, setState
+// componentDidUpdate, runs at every props or state change
+//      prevProps, prevState and compare to current
+// componentWillUnmount, runs once when component is going to be removed from DOM
+//      cleanUp of subscriptions (Listeners)
+// shouldComponentUpdate, runs at every props or state change
+//      defaults to true, if returns false, component will not update
