962544418895cf2b17b14a41a24e24fc17ab9617
Nairu Guan <guan.nairu@gmail.com>
2024-04-12 23:00:35 -0400

    update navbar link to use useNavigate from react-router-dom

--- demo/src/main/react/components/nav/Nav.js
+++ demo/src/main/react/components/nav/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
-
+    const nav = useNavigate()
     return (
         <nav className="navbar navbar-expand-lg bg-dark-subtle">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Web Dev</a>
+                <a className="navbar-brand" onClick={(e) => { e.preventDefault(); nav("/") }} href="/">Web Dev</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,16 +14,16 @@ const Nav = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="currentxkcdcomic">Current Comic</a>
+                            <a className="nav-link" onClick={(e) => { e.preventDefault(); nav("/currentxkcdcomic") }} href="currentxkcdcomic">Current Comic</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href='pastxkcdcomic'>Past Comic</a>
+                            <a className="nav-link" onClick={(e) => { e.preventDefault(); nav("/pastxkcdcomic") }} href='pastxkcdcomic'>Past Comic</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href='today-nasa-picture'>Today's NASA Picture</a>
+                            <a className="nav-link" onClick={(e) => { e.preventDefault(); nav("/today-nasa-picture") }} href='today-nasa-picture'>Today's NASA Picture</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href='past-nasa-pictures'>Past NASA Pictures</a>
+                            <a className="nav-link" onClick={(e) => { e.preventDefault(); nav("/past-nasa-pictures") }} href='past-nasa-pictures'>Past NASA Pictures</a>
                         </li>
                     </ul>
                 </div>
